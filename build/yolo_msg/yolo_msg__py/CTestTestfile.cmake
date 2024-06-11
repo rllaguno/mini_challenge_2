ccca91adea8c695b8327c5b296459f644de42c9b
EugenioTrevino02 <116983239+EugenioTrevino02@users.noreply.github.com>
2024-06-10 22:39:40 -0600

    Revert "Restore Final Challenge"

--- build/yolo_msg/yolo_msg__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/puzzlebot/github/mini_challenges_eq5/build/yolo_msg/yolo_msg__py
-# Build directory: /home/puzzlebot/github/mini_challenges_eq5/build/yolo_msg/yolo_msg__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
